a1dd3b9198e46a4f8f080f05dc41a9b862bb7c95
Duncan Dickinson <dedickinson@users.noreply.github.com>
2020-03-19 09:43:20 +1000

    Sorting out some isort issues

--- src/valiant/repositories/__init__.py
+++ src/valiant/repositories/__init__.py
@@ -1,9 +1,5 @@
 """Classes related to working with Python package repositories."""
 
-from .exceptions import (
-    PackageNotFoundException,
-    RepositoryException,
-    ValidationError,
-)
+from .exceptions import PackageNotFoundException, RepositoryException, ValidationError
 from .model import ArtifactMetadata, PackageMetadata
 from .repository import BaseRepository
